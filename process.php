b94b6490cedbcd3a3c55e00b61d4e0cf910c0a0c
Veekson <139856301+Veekson@users.noreply.github.com>
2023-07-18 21:10:54 +0100

    Update process.php

--- process.php
+++ process.php
@@ -15,10 +15,12 @@
     // Query
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
+    $gender = $_POST['gender'];
+    $dateofbirth = $_POST['date_of_birth'];
     $email = $_POST['email'];
 
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('$firstname', '$lastname', '$email')";
+    $sql = "INSERT INTO users (firstname, lastname, gender, date_of_birth, email) 
+    VALUES ('onugh', 'ogholo', 'm', '2004-03-18', 'emperordewise@gmail.com')";
 
     $result = $con->query($sql);
 
